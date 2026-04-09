6e1f9e7da43f7daada5f0303c749763b246eac6d
Aaron <blakugb@gmail.com>
2026-04-09 17:43:44 -0500

    Creating employee code variable that is assigned a string about a code of the employee

--- build-an-employee-profile-generator.py
+++ build-an-employee-profile-generator.py
@@ -13,3 +13,4 @@ position = "Data Analyst"
 salary = 75000
 employee_card = f"Employee: {full_name} | Age: {employee_age} | Position: {position} | Salary: {salary}"
 print(employee_card)
+employee_code = "DEV-2026-JD-001"
