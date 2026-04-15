31f770941908e765131134866bf33be7e2db57d1
Aaron <blakugb@gmail.com>
2026-04-14 23:35:52 -0500

    Figuring out the long numeric result displayed by round function only displaying 2 decimal that represent the cents

--- build-bill-splitter.py
+++ build-bill-splitter.py
@@ -8,10 +8,10 @@ desserts = 39.39
 drinks = 64.21
 
 running_total += appetizers + main_courses + desserts + drinks
-print(f"Total bill so far {running_total}")
+print(f"Total bill so far {round(running_total, 2)}")
 
 tip = running_total * 0.25
-print(f"Tip amount: {tip}")
+print(f"Tip amount: {round(tip, 2)}")
 
 final_bill = running_total / num_of_friends
-print(f"Bill per person: {final_bill}")
+print(f"Bill per person: {round(final_bill, 2)}")
